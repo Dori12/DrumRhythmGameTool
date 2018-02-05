57b3a95ebdc2bb1988d7bdaec1757eed618e4f62
DESKTOP-GM8NRCF\sunlight <sunlight1994@imrlab.hoseo.edu>
2018-02-05 22:37:33 +0900

    no message

--- DrumRhythmGodGameTool/Assets/Scripts/SoundScript.cs
+++ DrumRhythmGodGameTool/Assets/Scripts/SoundScript.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using UnityEngine.EventSystems;
+
+[Serializable]
+public class NoteData : IComparable
+{
+    public string name;
+    public float time;
+
+    public int CompareTo(object other)
+    {
+        if ((other is NoteData) == false) {
+            return 0;
+        }
+
+        return time.CompareTo((other as NoteData).time);
+    }
+}
 
 public class SoundScript : MonoBehaviour {
 
-    [SerializeField]
+    public GameObject ListNote;
+
     private AudioSource audioFile;
-    [SerializeField]
     private Text audioName;
-    [SerializeField]
     private Text currentTime;
-    [SerializeField]
     private Slider audioRemainTime;
+    private InputField audioPitch;
+
+    private float pitch;
+
+    private PriorityQueue<NoteData> noteDatas;
+    
 
     // Use this for initialization
     void Start () {
@@ -20,13 +43,94 @@ public class SoundScript : MonoBehaviour {
         audioName = GameObject.Find("AudioFileNameText").GetComponent<Text>();
         audioRemainTime = GetComponentInChildren<Slider>();
         currentTime = GameObject.Find("CurrentTimeText").GetComponent<Text>();
+        audioPitch = GetComponentInChildren<InputField>();
+        pitch = 0.0f;
+        noteDatas = new PriorityQueue<NoteData>();
 
         audioName.text = audioFile.clip.name;
-        currentTime.text = "" + audioFile.time;
-	}
+        currentTime.text = audioFile.time.ToString();
+
+        audioRemainTime.maxValue = audioFile.clip.length;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        currentTime.text = "" + audioFile.time;
+        CurrentTimePrint();
+        PitchControl();
+        SliderControl();
+    }
+
+    void CurrentTimePrint()
+    {
+        currentTime.text = audioFile.time + " / " + audioFile.clip.length;
+    }
+
+    void PitchControl()
+    {
+        bool parsed = float.TryParse(audioPitch.text, out pitch);
+        if (!parsed)
+        {
+            pitch = 0.0f;
+        }
+        audioFile.pitch = pitch;
+    }
+
+    void SliderControl()
+    {
+        if(!audioFile.isPlaying)
+        {
+            audioFile.time = audioRemainTime.value;
+        }
+        else
+        {
+            audioRemainTime.value = audioFile.time;
+        }
+    }
+
+    public void OnPlayButton()
+    {
+        audioFile.Play();
+    }
+
+    public void OnStopButton()
+    {
+        audioFile.Pause();
+    }
+
+    public void AudioTimeSet(float time)
+    {
+        audioFile.time = time;
+    }
+
+    public void ClickNote(string noteName)
+    {
+        NoteData notedata = new NoteData
+        {
+            name = noteName,
+            time = audioFile.time
+        };
+
+        noteDatas.Add(notedata);
+    }
+
+    public void Save()
+    {
+        string toJson = JsonHelper.ToJson<NoteData>(noteDatas.GetItemList().ToArray(), prettyPrint: true);
+        File.WriteAllText(Application.dataPath + "/Saves/data.json", toJson);
+    }
+
+    public void Load()
+    {
+        string jsonString = File.ReadAllText(Application.dataPath + "/Saves/data.json");
+        NoteData[] data = JsonHelper.FromJson<NoteData>(jsonString);
+        noteDatas.Clear();
+        for(int i=0; i<data.Length; i++)
+        {
+            noteDatas.Add(data[i]);
+            GameObject listNote = Instantiate(ListNote, GameObject.Find("Content").GetComponent<Transform>(), false);
+            listNote.GetComponent<Transform>().Translate(new Vector3(0, -30 * i));
+            listNote.GetComponentInChildren<Text>().text = noteDatas.GetItemList()[i].time + " / " + noteDatas.GetItemList()[i].name;
+        }
     }
 }
